4f5f2cb085e01532a00c60331fa918decd331904
Hugo MacDermott-Opeskin <hugo.macdermottopeskin@kekulai.com>
2025-12-24 11:39:42 +1100

    remove strange vendored paths that dont work

--- src/strain_relief/__init__.py
+++ src/strain_relief/__init__.py
@@ -2,21 +2,12 @@ from pathlib import Path
 
 from strain_relief.compute_strain import compute_strain
 
-# Directories
-project_dir: Path = Path(__file__).resolve().parents[2]
-src_dir: Path = project_dir / "src"
-test_dir: Path = project_dir / "tests"
-data_dir: Path = project_dir / "data"
-
-# the above don't work on pip installs, so adding this:
 # Current directory
-current_dir: Path = Path(__file__).resolve().parent
-config_dir: Path = current_dir / "hydra_config"
+_current_dir: Path = Path(__file__).resolve().parent
+# Configuration directory path
+config_dir: Path = _current_dir / "hydra_config"
 
 __all__ = [
     "compute_strain",
-    "project_dir",
-    "src_dir",
-    "test_dir",
-    "data_dir",
+    "config_dir"
 ]
