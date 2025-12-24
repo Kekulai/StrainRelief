66546bb8e1edeaa8ba3e11f66fe2873fb0fc16d4
Hugo MacDermott-Opeskin <hugomacdermott@gmail.com>
2025-12-24 21:18:49 +1100

    Fix vendored pathing for pip installs (#3)

--- src/strain_relief/__init__.py
+++ src/strain_relief/__init__.py
@@ -2,17 +2,12 @@ from pathlib import Path
 
 from strain_relief.compute_strain import compute_strain
 
-# Directories
-project_dir: Path = Path(__file__).resolve().parents[2]
-src_dir: Path = project_dir / "src"
-test_dir: Path = project_dir / "tests"
-data_dir: Path = project_dir / "data"
-config_dir: Path = src_dir / "strain_relief/hydra_config"
+# Current directory
+_current_dir: Path = Path(__file__).resolve().parent
+# Configuration directory path
+config_dir: Path = _current_dir / "hydra_config"
 
 __all__ = [
     "compute_strain",
-    "project_dir",
-    "src_dir",
-    "test_dir",
-    "data_dir",
+    "config_dir"
 ]
